75de5a14962f36a9e524bc56ae31e91ba6434302
~~SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2024-04-03 17:09:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	s=s.trim();
+	let idx = s.indexOf(' ');
+	let str = idx != -1 ? s.substr(0,idx) : s; 
+	return str;
+	
 }
 
 // Do not change the code below
